739f0703c44a5af8eff7182e0c9cb74fa03b0933
Ata Barzegar <atabarzegar79@gmail.com>
2022-07-11 09:19:22 +0430

    updating vote view

--- resources/views/vote.blade.php
+++ resources/views/vote.blade.php
@@ -5,45 +5,52 @@
 
 @endsection
 @section('content')
-    <p>Please give your Vote and Comment to breakfast with name of << {{$breakfast->name}} >> that served at   {{$breakfast->created_at}}   by
-        {{$breakfast->user->name}}!  </p>
-<div style="text-align: center">
-    <form class="user" method="POST"  action="{{route('breakfsatvotes.users.store',$breakfast->id)}}" style="display: block">
-        @csrf
-        @method('POST')
-        <div class="form-group">
-            <input type="number"
-                   style="width: 300px"
-                   class="form-control form-control-user"
-                   id="rate"
-                   name="rate"
-                   value="{{old('rate')}}"
-                   aria-describedby="emailHelp"
-                   placeholder="Enter your Rate.. ">
+    <p>Please give your Vote and Comment to breakfast with this name :</p>
+    <p>Breakfast Name : {{$breakfast->name}}</p>
+    <p>Date of Breakfast  : {{$breakfast->created_at}}  </p>
+    <p>Done by : {{$breakfast->user->name}}   </p>
+    <hr>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="p-5">
+                <form class="user" method="POST"  action="{{route('breakfsatvotes.users.store',$breakfast->id)}}" style="display: block">
+                    @csrf
+                    @method('POST')
+                    <div class="form-group">
+                        <input type="number"
+                               style="width: 300px"
+                               class="form-control form-control-user"
+                               id="rate"
+                               name="rate"
+                               value="{{old('rate')}}"
+                               aria-describedby="emailHelp"
+                               placeholder="Enter your Rate.. ">
+                    </div>
+                    @error('rate')
+                    <p style="color: red">
+                    {{$message}}
+                    </p>
+                    @enderror
+
+
+                    <div class="form-group">
+                        <textarea type="text"
+                                  style="width: 300px"
+                                class="form-control form-control-user"
+                                id="description"
+                                name="description"
+                                value="{{old('description')}}"
+                                aria-describedby="emailHelp"
+                                placeholder="Enter your Description "></textarea>
+                    </div>
+                    @error('description')
+                        <p style="color: red">
+                            {{$message}}
+                        </p>
+                    @enderror
+                    <input class="btn btn-primary btn-user btn-block" type="submit" value="Submit Rate" style="width: 300px;align-content: center">
+                </form>
+            </div>
         </div>
-        @error('rate')
-        <p style="color: red">
-            {{$message}}
-        </p>
-        @enderror
-
-
-        <div class="form-group">
-            <textarea type="text"
-                   style="width: 300px"
-                   class="form-control form-control-user"
-                   id="description"
-                   name="description"
-                   value="{{old('description')}}"
-                   aria-describedby="emailHelp"
-                   placeholder="Enter your Description "></textarea>
-        </div>
-        @error('description')
-        <p style="color: red">
-            {{$message}}
-        </p>
-    @enderror
-
-        <input class="btn btn-primary btn-user btn-block" type="submit" value="Submit Rate" style="width: 300px;align-content: center">
-</div>
+    </div>
 @endsection
