7375e5f9ddd39c2e9409ab5427a2e2c4fed1eba0
Ata Barzegar <atabarzegar79@gmail.com>
2022-07-17 11:26:53 +0430

    make a new vakidation rule for checking users

---
+++ app/Rules/breakfastMakers.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\User;
+use Illuminate\Contracts\Validation\Rule;
+
+class breakfastMakers implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $all_users = User::all() ;
+        $all_users_id = [] ;
+
+        foreach ($all_users as $user){
+            $all_users_id[] = (string)$user->id ;
+        }
+        foreach ($value as $id){
+            if(!in_array($id , $all_users_id)){
+               return  false ;
+            }
+        }
+        return  true ;
+
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return ' you have selected wrong :attribute  ';
+    }
+}
