941b05e56e694e1801a9b6956a7894f2d348148e
Ata Barzegar <atabarzegar79@gmail.com>
2022-07-22 13:46:46 +0430

    add Dto and Dto factory(just from model method) for Breakfast model

---
+++ app/Dtos/BreakfastDtoFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Dtos;
+
+use App\Models\Breakfast;
+
+
+class BreakfastDtoFactory
+{
+    public function fromModel(Breakfast $model): BreakfastDto
+    {
+        return  new BreakfastDto(
+            $model->id ,
+            $model->name ,
+            $model->name ,
+            $model->description ,
+            $model->created_at ,
+            $model->users ,
+            $model->userRate() );
+    }
+
+
+
+}
