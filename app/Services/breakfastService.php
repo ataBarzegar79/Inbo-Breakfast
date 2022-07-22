ba7a6ce420160a70f808e277ceda80c0974ae1fe
Ata Barzegar <atabarzegar79@gmail.com>
2022-07-22 13:10:46 +0430

    complete brekfast service interface

--- app/Services/breakfastService.php
+++ app/Services/breakfastService.php
@@ -1,3 +1,39 @@
 <?php
 namespace App\Services ;
 
+interface breakfastService{
+/**
+ * @return UserDTO[]
+ */
+public function create():array ;
+
+/**
+ * @param storeBreakfastRequest $request
+ * @return void
+ */
+public function store(storeBreakfastRequest $request):void ;
+
+
+/**
+ * @param int $user_id
+ * @return BreakfastDto[]
+ */
+public function edit(int $breakfast_id):array;
+
+
+/**
+ * @param  int $breakfast_id
+ * @param storeBreakfastRequest $request
+ * @return
+ */
+public function update(storeBreakfastRequest $request ,int $breakfast_id):void ;
+
+
+
+/**
+ * @param int $breakfast_id
+ */
+public function destroy(int $breakfast_id):void ;
+
+}
+
