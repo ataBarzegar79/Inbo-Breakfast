d74519a18c082d0dcf830096d646404cdfd94984
Ata Barzegar <atabarzegar79@gmail.com>
2022-07-23 10:38:19 +0430

    add dockblock

--- app/Dtos/UserBreakfastDto.php
+++ app/Dtos/UserBreakfastDto.php
@@ -4,6 +4,11 @@ namespace App\Dtos ;
 use App\Models\Breakfast;
 
 class UserBreakfastDto{
+    /**
+     * @param int $id
+     * @param string $name
+     * @param float $average  gives average participating for each user
+     */
     public function __construct(
         public int          $id,
         public string       $name,
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,79 +1,92 @@
 @extends('layout.app')
 
 @section('content')
-<div class="card shadow mb-4">
-    <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Breakfasts Done!</h6>
-    </div>
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Date of Operation</th>
-                    <th>Done by</th>
-                    <th>Rate</th>
-                    <th>Your Rate</th>
-                    <th>Your comment</th>
-                    @can('is_admin')
-                    <th>Functionalities </th>
-                    @endcan
-                </tr>
-                </thead>
+{{--<div class="card shadow mb-4">--}}
+{{--    <div class="card-header py-3">--}}
+{{--        <h6 class="m-0 font-weight-bold text-primary">Breakfasts Done!</h6>--}}
+{{--    </div>--}}
+{{--    <div class="card-body">--}}
+{{--        <div class="table-responsive">--}}
+{{--            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">--}}
+{{--                <thead>--}}
+{{--                <tr>--}}
+{{--                    <th>Name</th>--}}
+{{--                    <th>Description</th>--}}
+{{--                    <th>Date of Operation</th>--}}
+{{--                    <th>Done by</th>--}}
+{{--                    <th>Rate</th>--}}
+{{--                    <th>Your Rate</th>--}}
+{{--                    <th>Your comment</th>--}}
+{{--                    @can('is_admin')--}}
+{{--                    <th>Functionalities </th>--}}
+{{--                    @endcan--}}
+{{--                </tr>--}}
+{{--                </thead>--}}
 
-                <tbody>
-                <tr>
-                @foreach($breakfasts as $breakfast)
+{{--                <tbody>--}}
+{{--                <tr>--}}
+{{--                @foreach($breakfasts as $breakfast)--}}
 
-                    <td>{{$breakfast->name}}</td>
-                    <td>{{$breakfast->description}}</td>
-                    <td>{{$breakfast->persian}}</td>
-                    <td>@foreach($breakfast->users as $user)
-                        {{$user ->name}} |
-                        @endforeach
-                    </td>
+{{--                    <td>{{$breakfast->name}}</td>--}}
+{{--                    <td>{{$breakfast->description}}</td>--}}
+{{--                    <td>{{$breakfast->created_at}}</td>--}}
+{{--                    <td>@foreach($breakfast->users as $user)--}}
+{{--                        {{$user ->name}} |--}}
+{{--                        @endforeach--}}
+{{--                    </td>--}}
+{{--                        <td>{{$breakfast->users}}</td>--}}
+{{--                    <td>{{$breakfast->avareageRate()}}</td>--}}
+{{--                    <td>--}}
+{{--                        @if( $breakfast->userRate() !== null )--}}
+{{--                            {{$breakfast->userRate()['rate']}}--}}
+{{--                        @else--}}
+{{--                            <p> Vote! From This  <a href='{{route('breakfsatvotes.vote.create', $breakfast->id )}}'>Link</a> .</p>--}}
+{{--                        @endif--}}
+{{--                    </td>--}}
+{{--                    <td>--}}
+{{--                        @if($breakfast->userRate() !== null)--}}
+{{--                            {{$breakfast->userRate()['description']}}--}}
+{{--                        @endif--}}
+{{--                    </td>--}}
+{{--                    @can('is_admin')--}}
+{{--                    <td>--}}
+{{--                        <div>--}}
+{{--                            <form  method="POST" action="{{route('breakfast.delete' , $breakfast->id)}}" >--}}
 
-                    <td>{{$breakfast->avareageRate()}}</td>
-                    <td>
-                        @if( $breakfast->userRate() !== null )
-                            {{$breakfast->userRate()['rate']}}
-                        @else
-                            <p> Vote! From This  <a href='{{route('breakfsatvotes.vote.create', $breakfast->id )}}'>Link</a> .</p>
-                        @endif
-                    </td>
-                    <td>
-                        @if($breakfast->userRate() !== null)
-                            {{$breakfast->userRate()['description']}}
-                        @endif
-                    </td>
-                    @can('is_admin')
-                    <td>
-                        <div>
-                            <form  method="POST" action="{{route('breakfast.delete' , $breakfast->id)}}" >
+{{--                                @csrf--}}
+{{--                                {{ method_field('DELETE') }}--}}
+{{--                                <button class="  btn btn-danger btn-circle btn-sm"  type="submit" >--}}
+{{--                                    <i class="fas fa-trash"></i>--}}
+{{--                                </button>  Delete--}}
+{{--                            </form>--}}
+{{--                        </div>--}}
+{{--                        <br>--}}
+{{--                        <div>--}}
+{{--                            <a href="{{route('breakfast.update' ,$breakfast->id )}}" class="btn btn-warning btn-circle btn-sm">--}}
+{{--                                <i class="fas fa-exclamation-triangle"></i>--}}
+{{--                            </a>   Update--}}
+{{--                        </div>--}}
+{{--                    </td>--}}
+{{--                        @endcan--}}
+{{--                </tr>--}}
 
-                                @csrf
-                                {{ method_field('DELETE') }}
-                                <button class="  btn btn-danger btn-circle btn-sm"  type="submit" >
-                                    <i class="fas fa-trash"></i>
-                                </button>  Delete
-                            </form>
-                        </div>
-                        <br>
-                        <div>
-                            <a href="{{route('breakfast.update' ,$breakfast->id )}}" class="btn btn-warning btn-circle btn-sm">
-                                <i class="fas fa-exclamation-triangle"></i>
-                            </a>   Update
-                        </div>
-                    </td>
-                        @endcan
-                </tr>
-
-                @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
+{{--                @endforeach--}}
+{{--                </tbody>--}}
+{{--            </table>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+@foreach($breakfasts as $breakfast)
+    {{$breakfast->name}}
+    <br>
+    {{$breakfast->description}}
+    <br>
+    {{$breakfast->created_at}}
+    <br>
+    {{$breakfast->averageRate}}
+    <br>
+    @foreach($breakfast->users as  $user)
+        {{$user->name}}
+    @endforeach
+@endforeach
 @endsection
