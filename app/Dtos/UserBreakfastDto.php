3368c1fd5427d7651020c463136eadee4abb9284
Ehsan Aghayari <ehsanaghayari313@gmail.com>
2022-07-26 10:55:43 +0430

    done cleaning-up UserBreakfastDto.php

--- app/Dtos/UserBreakfastDto.php
+++ app/Dtos/UserBreakfastDto.php
@@ -1,21 +1,21 @@
 <?php
-namespace App\Dtos ;
 
-use App\Models\Breakfast;
-//fixme cleanup unused imports
+namespace App\Dtos;
 
-class UserBreakfastDto{
+//fixme cleanup unused imports *done
+
+class UserBreakfastDto
+{
     /**
      * @param int $id
      * @param string $name
-     * @param float $average  gives average participating for each user
+     * @param float $average gives average participating for each user
      */
     public function __construct(
-        public int          $id,
-        public string       $name,
-        public float        $average ,
+        public int    $id,
+        public string $name,
+        public float  $average,
     )
     {
     }
 }
-
