ccc8a305eb0960eceede9108a70eaed1448afdf0
Ehsan Aghayari <ehsanaghayari313@gmail.com>
2022-07-26 11:48:19 +0430

    done cleaning-up LoginRequest.php

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -3,18 +3,19 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use JetBrains\PhpStorm\ArrayShape;
 
-//fixme start class names with UpperCase
-class loginRequest extends FormRequest
+//fixme start class names with UpperCase *done
+class LoginRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
-    //fixme define return type for functions
+    //fixme define return type for functions *done
     //todo remove unused methods
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -24,13 +25,14 @@ class loginRequest extends FormRequest
      *
      * @return array<string, mixed>
      */
-    //todo use array shape annotation where ever is possible
-    //fixme define return type for functions
-    public function rules()
+    //todo use array shape annotation where ever is possible *done
+    //fixme define return type for functions *done
+    #[ArrayShape(['name' => "string[]", 'password' => "string[]"])] public function rules(): array
     {
         return [
             'name' => ['required', 'max:255'],
             'password' => ['required'],
         ];
     }
+
 }
