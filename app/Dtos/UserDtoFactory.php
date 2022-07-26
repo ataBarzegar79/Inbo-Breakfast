9256137b64ec13b3f15af94127e4f6e531b3d4a3
Ehsan Aghayari <ehsanaghayari313@gmail.com>
2022-07-26 10:59:39 +0430

    done some cleaning-up UserDtoFactory.php there is still some work to do

--- app/Dtos/UserDtoFactory.php
+++ app/Dtos/UserDtoFactory.php
@@ -6,24 +6,24 @@ use App\Models\User;
 use Carbon\Carbon;
 use Morilog\Jalali\Jalalian;
 
-
 class UserDtoFactory
 {
-    //todo use static methods in dto facilities
-    public function fromModel(User $user): UserDto
+    //todo use static methods in dto facilities *done
+    public static function fromModel(User $user): UserDto
     {
         //fixme use dtos instead of maps for data transferring
-        $user_rate = $user->performance()['rate'];//fixme use camelcase for variable names
-        $user_performance_color = $user->performance()['color'];
+        $userRate = $user->performance()['rate'];//fixme use camelcase for variable names *done
 
-        $user_avatar = $user->viewAvatar();
+        $userPerformanceColor = $user->performance()['color'];
+
+        $userAvatar = $user->viewAvatar();
 
         $averageParticipating = $user->averAgeParticipating();
 
         $countBreakfast = $user->countBreakfasts();
 
         // persianFormat helper class did not worked correctly, I had to handle it DASTY
-        $created_at = Jalalian::fromCarbon(new Carbon($user->created_at))->format('%A, %d %B %Y');//fixme use camelcase for variable names
+        $createdAt = Jalalian::fromCarbon(new Carbon($user->created_at))->format('%A, %d %B %Y');//fixme use camelcase for variable names
 
 
         return new UserDto(
@@ -31,11 +31,11 @@ class UserDtoFactory
             $user->name,
             $user->email,
             $user->password,
-            $created_at,
+            $createdAt,
             $user->is_admin,
-            $user_avatar,
-            $user_rate,
-            $user_performance_color,
+            $userAvatar,
+            $userRate,
+            $userPerformanceColor,
             $averageParticipating,
             $countBreakfast,
         );
