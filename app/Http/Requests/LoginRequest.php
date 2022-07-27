7f9bfdf5d0b3148a10b74fb30202444e17fddd1c
Ata Barzegar <atabarzegar79@gmail.com>
2022-07-27 12:50:21 +0430

    remove athorize method in LoginRequest.php

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -8,17 +8,7 @@ use JetBrains\PhpStorm\ArrayShape;
 //fixme start class names with UpperCase *done
 class LoginRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    //fixme define return type for functions *done
-    //todo remove unused methods
-    public function authorize(): bool
-    {
-        return true;
-    }
+
 
     /**
      * Get the validation rules that apply to the request.
