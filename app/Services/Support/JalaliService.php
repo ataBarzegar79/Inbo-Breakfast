a0228d7239009f3221aa84e5070375f8c7dc19a5
Ata Barzegar <atabarzegar79@gmail.com>
2022-07-27 08:45:05 +0430

    initilize jalali Service interface and Concrete class .

---
+++ app/Services/Support/JalaliService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\Support ;
+
+
+interface  JalaliService {
+
+}
---
+++ app/Services/Support/JalaliServiceConcrete.php
@@ -0,0 +1,8 @@
+<?php
+
+
+namespace  App\Services\Support ;
+
+class JalaliServiceConcrete implements JalaliService{
+
+}
