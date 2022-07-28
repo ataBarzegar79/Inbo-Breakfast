19d034cd61c2d5d08c1fd66ca10572f4f7fd5d3a
Ehsan Aghayari <ehsanaghayari313@gmail.com>
2022-07-28 07:53:23 +0430

    some work on UserDtoFactory.php

--- app/Dtos/UserDtoFactory.php
+++ app/Dtos/UserDtoFactory.php
@@ -3,31 +3,16 @@
 namespace App\Dtos;
 
 use App\Models\User;
-use App\Services\UserSupportService;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
 use Morilog\Jalali\Jalalian;
 
 class UserDtoFactory
 {
     //todo use static methods in dto facilities *done
-    public static function fromModel(User $user, UserSupportService $service): UserDto
+    public static function fromModel(User $user, string $avatar, float|string $userRate, string $userPerformanceColor, float $averageParticipating, $countBreakfast): UserDto
     {
-
-        $avatar = $service->viewAvatar($user->id);
-
-        $userRate = $service->performance($user->id);
-
-        $userPerformanceColor = $service->performanceColor($user->id, $userRate);
-
-        $averageParticipating = $service->averAgeParticipating($user->id);
-
-        $countBreakfast = $service->countBreakfasts($user->id);
-
         // persianFormat helper class did not work correctly, I had to handle it DASTY
         $createdAt = Jalalian::fromCarbon(new Carbon($user->created_at))->format('%A, %d %B %Y');//fixme use camelcase for variable names
-
 
         return new UserDto(
             $user->id,
