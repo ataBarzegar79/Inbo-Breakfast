b2648e14fbc5583ca8d7e22811725b0ae44f004a
Ehsan Aghayari <ehsanaghayari313@gmail.com>
2022-07-30 15:20:45 +0430

    implement Rate service base on DTO insteadof Requests

--- app/Services/Rate/RateCreateService.php
+++ app/Services/Rate/RateCreateService.php
@@ -5,6 +5,7 @@ namespace App\Services\Rate;
 use App\Dtos\Breakfast\BreakfastDtoDoerFactory;
 use App\Dtos\Breakfast\BreakfastUpdateDto;
 use App\Dtos\Breakfast\BreakfastUpdateDtoFactory;
+use App\Dtos\VoteRequestDto;
 use App\Http\Requests\StoreVoteRequest;
 use App\Models\Breakfast;
 use App\Models\Rate;
@@ -25,13 +26,13 @@ class  RateCreateService implements RateService
     }
 
     //fixme use camelcase for function parameters : Done
-    public function store(StoreVoteRequest $request, int $breakfastId): void
+    public function store(VoteRequestDto $dto, int $breakfastId): void
     {
         $user = auth()->user();
         Rate::create([ //todo clean unused variables :Done
             'user_id' => $user->id, // todo Ehsan: change this to Auth::id()
-            'rate' => $request->rate,
-            'description' => $request->description,
+            'rate' => $dto->rate,
+            'description' => $dto->description,
             'breakfast_id' => $breakfastId
         ]);
     }
