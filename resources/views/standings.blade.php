8513d2a12af0b29a151070c3b57f9fb0da8d86b5
Ehsan Aghayari <ehsanaghayari313@gmail.com>
2022-08-01 11:10:12 +0430

    user tables some cleaning up

--- resources/views/standings.blade.php
+++ resources/views/standings.blade.php
@@ -5,40 +5,49 @@
 @section('content')
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Inbo Breakfast Standings  !  ({{ $usersAverage * 10 }}) average participating)</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Inbo Breakfast Standings ! ({{ $usersAverage * 10 }}) average
+                participating)</h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
-                    <tr>
+                    <tr style="text-align: center">
                         <th></th>
                         <th>Avatar</th>
                         <th>Name</th>
-                        <th>Joind From :</th>
+                        <th>Joined From:</th>
                         <th>Performance</th>
-                        <th>Has Role </th>
-                        <th>Avarage Participation</th>
+                        <th>Has Role</th>
+                        <th>Average Participation</th>
                     </tr>
                     </thead>
 
                     <tbody>
                     <tr>
                         @php
-                        $counter = 0 ;
+                            $counter = 0 ;
                         @endphp
                         @foreach($users->data as $user)
-                            <td>{{$counter+=1}}</td>
-                            <td> <img class= " center img-profile rounded-circle " width="60" height="60" src = {{$user->avatar}} alt=""> </td>
-                            <td>{{$user->name}}</td>
-                            <td class="text-center">{{$user->createdAt}}</td>
-                            <td class="text-center" style="background-color: {{-- $user->color --}}">{{$user->rate}}</td>
-                            <td> <span class="font-weight-bolder" >{{$user->countBreakfast}}</span>  breakfasts</td>
+                            <td style="text-align: center">{{$counter+=1}}</td>
+                            <td style="text-align: center">
+                                <img class=" center img-profile rounded-circle " width="60" height="60"
+                                     src={{$user->avatar}} alt="">
+                            </td>
+                            <td style="text-align: center">{{$user->name}}</td>
+                            <td class="text-center" style="text-align: center">{{$user->createdAt}}</td>
+                            <td class="text-center"
+                                style="background-color: {{$user->color}}; text-align: center">{{$user->rate}}</td>
+                            <td><span class="font-weight-bolder">{{$user->countBreakfast}}</span> breakfasts</td>
 
                             @if($user->averageParticipating > $usersAverage)
-                                <td class="alert-success text-center"><span class="font-weight-bold">{{$user->averageParticipating*10 }} XP</span></td>
+                                <td class="alert-success text-center" style="text-align: center">
+                                    <span class="font-weight-bold">{{$user->averageParticipating*10 }} XP</span>
+                                </td>
                             @else
-                                <td class="alert-danger text-center"><span class="font-weight-bold">{{$user->averageParticipating*10 }} XP</span></td>
+                                <td class="alert-danger text-center" style="text-align: center">
+                                    <span class="font-weight-bold">{{$user->averageParticipating*10 }} XP</span>
+                                </td>
                             @endif
                     </tr>
                     @endforeach
